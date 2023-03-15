476efdb542178878b0a88fcd7967a94ebc9f3d2b
Gini Stanzi <a.stanzione14@gmail.com>
2023-03-15 14:56:26 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ import Favorites from './components/favorite-list.js';
 
 function App() {
   return (
-    <div></div>
-  );
-}
+    <Router>
+    <div>
+      <Navbar />
+      {/* Wrap Route elements in a Routes component */}
+      <Routes basename='./components/'>
+        <Route path = './components/home-page/' element={<Home/>}/>
+        {/* Define routes using the Route component to render different page components at different paths */}
+        {/* Define a default route that will render the Home component */}
+        <Route path='./components/flight-results/' element={<FlightResults />} />
+        <Route path='/components/travel-inspo/' element={<TravelInspo/>} />
+        {/* Define a route that will have descendant routes */}
+        <Route path='/components/favorite-list/' element={<Favorites />} />
+      </Routes>
+    </div>
+  </Router>
+);
+};
 export default App;
