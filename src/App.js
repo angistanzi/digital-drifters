3e26515c1ee0b18e8be6428dd29b6abe309f0e24
Gini Stanzi <a.stanzione14@gmail.com>
2023-03-15 14:47:06 +0000

    created base skeleton for each component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar.js';
+import Home from './components/home-page.js';
+import TravelInspo from './components/travel-inspo.js';
+import FlightResults from './components/flight-results.js';
+import Favorites from './components/favorite-list.js';
 
-
-function App(){
-
-  return(
-    <div>
-
-    </div>
-  )
-
-};
-
+function App() {
+  return (
+    <div></div>
+  );
+}
 export default App;
---
+++ src/components/travel-inspo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TravelInspo (){
+
+    return(
+        <div></div>
+    );
+}
+
+export default TravelInspo;
