aa0106f9ee795e7f8f3d595bd917f68d827145d4
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-15 16:19:41 +0000

    initial commit

--- src/components/flight-results.js
+++ src/components/flight-results.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function FlightResults (){
+function FlightResults() {
 
-    return(
-        <div></div>
-    );
-}
+    return (
+            // Return a JSX element that represents the search results page for a flight search
+            <div>
+            <h1 class="display-4 text-primary">Search Results</h1>  {/* Header for the search results page */}
+            <div class="card">
+              <img
+                url="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+                class="card-img-top"
+                alt="Flight"/>  {/* Image of a flight */}
+              <div class="card-header bg-primary text-white">
+                <h2 class="h5">Flight 123</h2> {/* Flight number */}
+                <p class="text-muted">Departure: 10:00 AM</p>   {/* Departure time */}
+              </div>
+              <div class="card-body">
+                <p class="lead">$200</p>{/* Price of the flight */}               
+              </div>
+            </div>
+          </div>
+        );
+      }
 
 export default FlightResults;
