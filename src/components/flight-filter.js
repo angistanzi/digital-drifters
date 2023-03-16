996c257c4bed0b5bf565e52c17423a805ce8bbb9
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-16 16:08:54 +0000

    adding all extra filters to a dropdown to reduce cognitive overload on the screen

--- src/components/flight-filter.js
+++ src/components/flight-filter.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Row, Dropdown } from "react-bootstrap";
 import { Button } from 'react-bootstrap';
 
 function FlightFilter() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
     return (
         <Form>
             <Row>
@@ -25,8 +28,8 @@ function FlightFilter() {
                     <Form.Label>Return date</Form.Label>
                     <Form.Control type="date" />
                 </Form.Group>
-                 {/* Number of passengers input */}
-                 <Form.Group className="col-md-1" controlId="formPassengers">
+                {/* Number of passengers input */}
+                <Form.Group className="col-md-1" controlId="formPassengers">
                     <Form.Label>Passengers</Form.Label>
                     <Form.Control as="select" defaultValue="1">
                         <option>1</option>
@@ -37,48 +40,55 @@ function FlightFilter() {
                     </Form.Control>
                 </Form.Group>
             </Row>
-            <Row>
-                {/* Cabin class input */}
-                <Form.Group className="col-md-2" controlId="formCabinClass">
-                    <Form.Label>Cabin Class</Form.Label>
-                    <Form.Control as="select">
-                        <option>Economy</option>
-                        <option>Premium Economy</option>
-                        <option>Business</option>
-                        <option>First Class</option>
-                    </Form.Control>
-                </Form.Group>
-                {/* Direct Flights Only checkbox */}
-                <Form.Group className="col-md-2" controlId="formDirectFlightsOnly">
-                    <Form.Label>Direct Flights Only</Form.Label>
-                    <Form.Check type="checkbox" label="" />
-                </Form.Group>
-                {/* Flexible Dates checkbox */}
-                <Form.Group className="col-md-2" controlId="formFlexibleDates">
-                    <Form.Label>Flexible Dates</Form.Label>
-                    <Form.Check type="checkbox" label="" />
-                </Form.Group>
-                {/* Departure Time input */}
-                <Form.Group className="col-md-2" controlId="formDepartureTime">
-                    <Form.Label>Departure Time</Form.Label>
-                    <Form.Control as="select" defaultValue="Any">
-                        <option>Any</option>
-                        <option>Morning</option>
-                        <option>Afternoon</option>
-                        <option>Evening</option>
-                    </Form.Control>
-                </Form.Group>
-                {/* Price Range input */}
-                <Form.Group className="col-md-2" controlId="formPriceRange">
-                    <Form.Label>Price Range</Form.Label>
-                    <Form.Control type="number" placeholder="Min" />
-                    <Form.Control type="number" placeholder="Max" />
-                </Form.Group>
-                {/* Submit button */}
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Row>
+            {/* Extra filters dropdown */}
+            <Dropdown>
+                <Dropdown.Toggle variant="secondary" id="filter-dropdown" onClick={toggleDropdown}>
+                    Filters
+                </Dropdown.Toggle>
+                <Dropdown.Menu show={isOpen}>
+                    <Dropdown.Header>Filter by</Dropdown.Header>
+                    <Dropdown.Divider />
+                    {/* Cabin class input */}
+                    <Form.Group className="col-md-2" controlId="formCabinClass">
+                        <Form.Label>Cabin Class</Form.Label>
+                        <Form.Control as="select">
+                            <option>Economy</option>
+                            <option>Premium Economy</option>
+                            <option>Business</option>
+                            <option>First Class</option>
+                        </Form.Control>
+                    </Form.Group>
+                    {/* Direct Flights Only checkbox */}
+                    <Form.Group className="col-md-2" controlId="formDirectFlightsOnly">
+                        <Form.Label>Direct Flights Only</Form.Label>
+                        <Form.Check type="checkbox" label="" />
+                    </Form.Group>
+                    {/* Flexible Dates checkbox */}
+                    <Form.Group className="col-md-2" controlId="formFlexibleDates">
+                        <Form.Label>Flexible Dates</Form.Label>
+                        <Form.Check type="checkbox" label="" />
+                    </Form.Group>
+                    {/* Departure Time input */}
+                    <Form.Group className="col-md-2" controlId="formDepartureTime">
+                        <Form.Label>Departure Time</Form.Label>
+                        <Form.Control as="select" defaultValue="Any">
+                            <option>Any</option>
+                            <option>Morning</option>
+                            <option>Afternoon</option>
+                            <option>Evening</option>
+                        </Form.Control>
+                    </Form.Group>
+                    {/* Price Range input */}
+                    <Form.Group className="col-md-2" controlId="formPriceRange">
+                        <Form.Label>Price Range</Form.Label>
+                        <Form.Control type="number" placeholder="Min" />
+                        <Form.Control type="number" placeholder="Max" />
+                    </Form.Group></Dropdown.Menu>
+            </Dropdown>
+            {/* Submit button */}
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
         </Form>
     )
 };
