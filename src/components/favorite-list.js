660950d063de43c58076d202a01ae1a07539e6f3
Coni Hinojosa <coni@Conis-MBP.broadband>
2023-03-16 14:17:13 +0000

    made API.js file with new API

--- src/components/favorite-list.js
+++ src/components/favorite-list.js
@@ -3,17 +3,38 @@ import { Container, Row, Col, Form, Card, Button } from "react-bootstrap";
 import "./favorite-list.css";
 
 function Favorites() {
+  const [cityName, setCityName] = useState("");
+  const [visitedCities, setVisitedCities] = useState(
+    JSON.parse(localStorage.getItem("visitedCities")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("visitedCities", JSON.stringify(visitedCities));
+  }, [visitedCities]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getCityData(cityName).then((cityData) => {
+      setVisitedCities([...visitedCities, cityData]);
+    });
+    setCityName("");
+  };
+
   return (
     <Container className="py-5 my-5">
       <div className="text-center mb-5">
         <h1 className="display-4 fw-bold mb-5">FAVOURITES</h1>
         <Row className="justify-content-center">
           <Col lg="5">
-            <Form.Control
-              type="text"
-              placeholder="Write the cities you have visited"
-              aria-label="Search"
-            />
+            <Form onSubmit={handleSubmit}>
+              <Form.Control
+                type="text"
+                placeholder="Write the cities you have visited"
+                aria-label="Search"
+                onChange={(event) => setCityName(event.target.value)}
+                value={cityName}
+              />
+            </Form>
           </Col>
         </Row>
       </div>
@@ -22,54 +43,25 @@ function Favorites() {
           <div className="text-center mb-5">
             <h2>Places I've been</h2>
           </div>
-          <Card className="cityCard text-white">
-            <Card.Img
-              src="https://media-cdn.tripadvisor.com/media/photo-s/1c/c0/b1/4b/caption.jpg"
-              alt="Card image"
-            />
-            <div className="gradientDiv"></div>
 
-            <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-              <div className="d-inline-flex justify-content-between">
-                <Card.Title>London, United Kingdom</Card.Title>
-                <Button variant="warning" size="sm">
-                  Similar places
-                </Button>
-              </div>
-            </Card.ImgOverlay>
-          </Card>
-          <Card className="cityCard text-white">
-            <Card.Img
-              src="https://media-cdn.tripadvisor.com/media/photo-s/1c/c2/86/0e/caption.jpg"
-              alt="Card image"
-            />
-            <div className="gradientDiv"></div>
+          {visitedCities.map((city) => (
+            <Card key={city.id} className="cityCard text-white">
+              <Card.Img
+                src={city.image}
+                alt={`${city.name}, ${city.country}`}
+              />
+              <div className="gradientDiv"></div>
 
-            <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-              <div className="d-inline-flex justify-content-between">
-                <Card.Title>Paris, France</Card.Title>
-                <Button variant="warning" size="sm">
-                  Similar places
-                </Button>
-              </div>
-            </Card.ImgOverlay>
-          </Card>
-          <Card className="cityCard text-white">
-            <Card.Img
-              src="https://media-cdn.tripadvisor.com/media/photo-s/1c/c2/87/c8/caption.jpg"
-              alt="Card image"
-            />
-            <div className="gradientDiv"></div>
-
-            <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-              <div className="d-inline-flex justify-content-between">
-                <Card.Title>San Francisco, California</Card.Title>
-                <Button variant="warning" size="sm">
-                  Similar places
-                </Button>
-              </div>
-            </Card.ImgOverlay>
-          </Card>
+              <Card.ImgOverlay className="d-flex flex-column justify-content-end">
+                <div className="d-inline-flex justify-content-between">
+                  <Card.Title>{`${city.name}, ${city.country}`}</Card.Title>
+                  <Button variant="warning" size="sm">
+                    Similar places
+                  </Button>
+                </div>
+              </Card.ImgOverlay>
+            </Card>
+          ))}
         </Col>
         <Col lg="5">
           <div className="text-center mb-5">
