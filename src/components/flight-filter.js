f9d101ae05f36c72856dd42cf987bc5271f7107d
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-16 14:50:12 +0000

    added a submit button component from bootstrap

--- src/components/flight-filter.js
+++ src/components/flight-filter.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import { Button } from 'react-bootstrap';
 
 export default function FlightFilter() {
     return (
         <Form>
             <Form.Row>
+                {/* Departure location input */}
                 <Form.Group className="col-md-4" controlId="formDeparture">
                     <Form.Label>Departure</Form.Label>
                     <Form.Control type="text" placeholder="Enter departure city" />
                 </Form.Group>
+                {/* Arrival location input */}
                 <Form.Group className="col-md-4" controlId="formDestination">
                     <Form.Label>Destination</Form.Label>
                     <Form.Control type="text" placeholder="Enter destination city" />
                 </Form.Group>
+                {/* Flight dates input */}
                 <Form.Group className="col-md-2" controlId="formDepartureDate">
                     <Form.Label>Departure date</Form.Label>
                     <Form.Control type="date" />
@@ -23,6 +27,7 @@ export default function FlightFilter() {
                 </Form.Group>
             </Form.Row>
             <Form.Row>
+                {/* Number of passengers input */}
                 <Form.Group className="col-md-3" controlId="formPassengers">
                     <Form.Label>Passengers</Form.Label>
                     <Form.Control as="select" defaultValue="1">
@@ -33,6 +38,10 @@ export default function FlightFilter() {
                         <option>5+</option>
                     </Form.Control>
                 </Form.Group>
+                {/* Submit button */}
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </Form.Row>
         </Form>
     )
